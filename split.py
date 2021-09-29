ac00bc0ec5a78b0b61bd7074ac49c89551c24de6
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2021-09-30 00:23:54 +0900

    Update split.py

--- split.py
+++ split.py
@@ -2,11 +2,11 @@ import sys
 path = sys.argv[1]
 
 arr = []
-with open(path) as f:
+with open(path, encoding='utf-8') as f:
   s = f.read()
   arr = s.split("======")
 
 print(len(arr))
 for i in range(len(arr)):
-  with open('./CHAPTER{}.md'.format(i+1), mode='w') as f:
+  with open('./CHAPTER{}.md'.format(i+1), mode='w', encoding='utf-8') as f:
     f.write("  \n".join(arr[i].split('\n')))
